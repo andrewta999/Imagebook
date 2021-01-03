12901d6ad9891f08faa0436560d3c83c14a04bfb
andrewta999 <andrewta365@gmail.com>
2021-01-03 17:59:27 -0500

    added some middlewares

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: '*'}))
+
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+
+app.listen(3000, () => {
+    console.log("App is running")
+})

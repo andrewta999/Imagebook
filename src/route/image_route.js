36659891bc1412d5a2e70d94e43501c9b1838d27
andrewta999 <andrewta365@gmail.com>
2021-01-04 16:16:36 -0500

    some changes

--- src/route/image_route.js
+++ src/route/image_route.js
@@ -6,10 +6,10 @@ import user_controller from '../controller/user_controller'
 const router = express.Router()
 
 // upload a new image
-router.route('/image/new/:userId').post(user_controller.require_signin, image_controller.create_image)
+router.route('/image/new/:userId').post(user_controller.require_signin, user_controller.is_account, image_controller.create_image)
 
 // get all images of an user
-router.route('/image/:userId').get(user_controller.is_author, image_controller.list_by_user)
+router.route('/image/:userId').get(user_controller.require_signin, user_controller.is_author, image_controller.list_by_user)
 
 // delete an image
 router.route('/image/:imageId').delete(user_controller.require_signin, user_controller.has_authorization, 

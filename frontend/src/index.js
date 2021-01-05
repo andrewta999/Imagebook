268764372ee57375e691539dbba82fd6ae90ddb6
andrewta999 <andrewta365@gmail.com>
2021-01-05 13:06:27 -0500

    used hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </Router>
+    </HashRouter>
     ,
     document.getElementById('root')
 )
